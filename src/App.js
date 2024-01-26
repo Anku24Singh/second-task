61bb502316d770e64fbfe9df2b2bc0ecc4a6ec78
Anku Kumari <ankusinghcps@gmail.com>
2024-01-26 15:09:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css'; // Import your CSS file
+
+// function App() {
+//   return (
+//     <div className="outer-grid">
+//       {/* Outer Grid */}
+//       <div className="inner-grid">
+//         {/* Inner Grid */}
+//         <div className="grid-item">Item 1</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import './App.css'; // Import your CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   
+    <div className="outer-container">
+    
+      <div className="outer-grid">
+
+      </div>
+      <div className='outer-grid2'>
+      <b className='head'>SEO </b><b className='head2'>PACKAGES</b>
+      <br></br>
+      <br></br>
+        Today, a website cannot do without search engine optimization. The strategy is the lifeline to bring you on the the firrast page and rank hugher than your computer.
+        You can no longer sign up for yoast SEO or subscribe to yet and expect results. It is time for customized and evolved approach thanks to thr updates in serach engine optimization.
+        <br></br><br></br>
+        We provide expert SEo service for small, medium, and enterprose-level companies.<br></br>
+        If you're not looking for SEO packages that deliver, discover what we have in store for you
+      </div>
+        <div className="inner-grid">
+          {/* Inner Grid */}
+         
+          <div className="grid-item"><p>REGULAR MEETINGS</p>
+          <p>TECHNICAL SEO</p>
+          <p> KEYWORD TRACKING</p>
+          <p>LOCAL SEO</p>
+          <p>MULTI-LOCATION SEO</p>
+          <p>REVIEW ACCELERATION</p>
+          <p> LOCAL SEO</p>
+          <p>COPYWRITING</p>
+          <p>LINK BUILDING</p>
+          
+          </div>
+        
+      </div>
     </div>
+    </>
   );
 }
 
